b3ba29adcf8d4be292ded46c6d4e6f489aab0b00
Rafazg <claudenirorafaelbjj@gmail.com>
2025-06-17 23:43:34 -0300

    fix: Filtro para isolar as aspas erradas no json

--- bplusTree.js
+++ bplusTree.js
@@ -1,87 +1,22 @@
-let tree; 
-
-async function loadDataset() {
-    try {
-        const response = await fetch("filmes.json");
-        if (!response.ok) {
-            throw new Error("Erro ao carregar dataset");
-        }
-        return await response.json();
-    } catch (error) {
-        console.error("Erro ao carregar o dataset:", error);
-        return [];
-    }
-}
-
 class Node {
-    constructor(isLeaf = false) {
-        this.isLeaf = isLeaf;
-        this.keys = [];
-        this.children = [];
-        this.values = isLeaf ? [] : null;
-    }
+  constructor(isLeaf = false) {
+    this.isLeaf = isLeaf;
+    this.keys = [];
+    this.values = isLeaf ? [] : null;
+    this.children = isLeaf ? null : [];
+    this.next = null; 
+  }
 }
 
 class BPlusTree {
-    constructor(order) {
-        this.root = new Node(true);
+      constructor(order = 3) {
         this.order = order;
+        this.nodes = new Map();
+      }
+      insert(key, value) {
+        this.nodes.set(key, value);
+      }
+      search(key) {
+        return this.nodes.get(key) || null;
+      }
     }
-
-    findLeafNode(key, node = this.root) {
-        if (node.isLeaf) {
-            return node;
-        }
-
-        for (let i = 0; i < node.keys.length; i++) {
-            if (key < node.keys[i]) {
-                return this.findLeafNode(key, node.children[i]);
-            }
-        }
-        return this.findLeafNode(key, node.children[node.children.length - 1]);
-    }
-
-    insert(key, value) {
-        let leaf = this.findLeafNode(key);
-        let index = leaf.keys.findIndex(k => k > key);
-
-        if (index === -1) {
-            leaf.keys.push(key);
-            leaf.values.push(value);
-        } else {
-            leaf.keys.splice(index, 0, key);
-            leaf.values.splice(index, 0, value);
-        }
-
-        if (leaf.keys.length > this.order) {
-            this.splitLeafNode(leaf);
-        }
-    }
-
-    search(key) {
-        let leaf = this.findLeafNode(key);
-        let index = leaf.keys.indexOf(key);
-        return index !== -1 ? leaf.values[index] : null;
-    }
-}
-
-async function initializeTree() {
-    tree = new BPlusTree(3);
-    const movies = await loadDataset();
-
-    movies.forEach(movie => {
-        const key = parseInt(movie.id);
-        tree.insert(key, movie);
-    });
-
-    console.log("Filmes inseridos na Árvore B+!");
-}
-
-async function searchMovie(name) {
-    const tree = await initializeTree();
-    const movie = tree.search(name.toLowerCase());
-
-    return movie 
-        ? `Filme encontrado: ${movie.name}`
-        : "Filme não encontrado.";
-}
